040bd12a942799fa91dbe801ea3dea475c0f1a08
thiagocolen <thiago.souzacolen@gmail.com>
2016-04-21 23:15:29 -0300

    Add a Build System and Todolist.txt

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,67 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var inject = require('gulp-inject');
+var clean = require('gulp-clean');
 
 
-//www folder
+//Clean WWW Folder 
+gulp.task('clean', function () {
+	return gulp.src('./www/', {read: false})
+		.pipe(clean());
+});
+
+// WWW Folder
 gulp.task('dependencies', function() {
-    // HTML
-    gulp.src('./dev/index.html')
+
+    // htmls
+    gulp.src('./dev/*.html')
+        .pipe(gulp.dest('./www/'));
+
+    // scripts
+    gulp.src('./bower_components/jquery/dist/jquery.js')
+        .pipe(gulp.dest('./www/scripts/'));
+
+    gulp.src('./bower_components/bootstrap/dist/js/bootstrap.js')
+        .pipe(gulp.dest('./www/scripts/'));
+
+    gulp.src('./bower_components/angular/angular.js')
+        .pipe(gulp.dest('./www/scripts/'));
+
+    gulp.src('./bower_components/angular-route/angular-route.js')
+        .pipe(gulp.dest('./www/scripts/'));
+
+    gulp.src('./bower_components/firebase/firebase.js')
+        .pipe(gulp.dest('./www/scripts/'));
+
+    gulp.src('./bower_components/angularfire/dist/angularfire.js')
+        .pipe(gulp.dest('./www/scripts/'));
+
+    //css
+    gulp.src('./bower_components/bootstrap/dist/css/bootstrap.css')
+        .pipe(gulp.dest('./www/styles/'));
+
+    //fonts 
+    gulp.src('./bower_components/bootstrap/dist/fonts/*.*')
+        .pipe(gulp.dest('./www/fonts/'));
+});
+
+// Inject dependencies in html
+gulp.task('inject',  function() {
+    var target = gulp.src('./www/index.html');
+    var sources = gulp.src([
+        './www/scripts/jquery.js',
+        './www/scripts/bootstrap.js',
+        './www/styles/bootstrap.css',
+        './www/scripts/angular.js',
+        './www/scripts/angular-route.js',
+        './www/scripts/firebase.js',
+        './www/scripts/angularfire.js'
+    ], {
+        read: false
+    });
+
+    return target.pipe(inject(sources, {relative: true}))
         .pipe(gulp.dest('./www/'));
 });
 
@@ -21,6 +76,19 @@ gulp.task('browser-sync', function() {
 });
 
 
-gulp.task('default', ['dependencies', 'browser-sync'], function() {
+gulp.task('default', ['browser-sync'], function() {
     gulp.watch(['dev/**/*'], ['inject', reload]);
 });
+
+
+
+
+// execute:
+// 1. clean
+// 2. dependencies
+// 3. inject
+// 4. default
+
+
+
+
