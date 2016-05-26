a024a3adae662fa36f79a7275728a498ae338f28
thiagocolen <thiago.souzacolen@gmail.com>
2016-05-26 17:11:57 -0300

    adding edit panel, this is not finished

--- dev/scripts/controllers.js
+++ dev/scripts/controllers.js
@@ -23,10 +23,8 @@ fortheloveofmoneyControllers.controller("CategoriesCtrl", ["$scope", "$firebaseA
 ]);
 
 
-fortheloveofmoneyControllers.controller("HomeCtrl", ["$scope", "$firebaseArray", "$firebaseObject", "Ref", "$filter", 
+fortheloveofmoneyControllers.controller("HomeCtrl", ["$scope", "$firebaseArray", "$firebaseObject", "Ref", "$filter",
     function($scope, $firebaseArray, $firebaseObject, Ref, $filter) {
-
-        console.log('HomeCtrl');
 
         Ref.child('/transactions').orderByChild("date").on("value", function(snapshot, $filter) {
 
@@ -41,7 +39,7 @@ fortheloveofmoneyControllers.controller("HomeCtrl", ["$scope", "$firebaseArray",
 
             var line = [];
             $scope.values = [];
-  
+
 
             snapshot.forEach(function(data) {
                 // console.log("key: " + data.key() + " : " + data.val().date + " : " + data.val().value);
@@ -50,14 +48,11 @@ fortheloveofmoneyControllers.controller("HomeCtrl", ["$scope", "$firebaseArray",
                 var valor = data.val().value;
                 var label = data.val().date;
 
-                // console.log('date: ', label.substr(0, 10));
-
                 line.push(valor);
                 $scope.labels.push(label.substr(0, 10));
             });
 
             $scope.data.push(line);
-
         }, function(errorObject) {
             console.log("The read failed: " + errorObject.code);
         });
@@ -65,22 +60,86 @@ fortheloveofmoneyControllers.controller("HomeCtrl", ["$scope", "$firebaseArray",
         $scope.categories = $firebaseArray(Ref.child('/categories'));
         $scope.transactions = $firebaseArray(Ref.child('/transactions'));
 
-        $scope.saveTransaction = function() {
-            var list = $firebaseArray(Ref.child('/transactions'));
-            $scope.newTransaction.date = $scope.dateInput.toJSON();
+        $scope.saveTransaction = function(type) {
 
-            list.$add($scope.newTransaction).then(function(ref) {
-                var id = ref.key();
-                // list.$indexFor(id); // returns location in the array
-                // console.log(id);
-                $scope.newTransaction = '';
-                $('#tabindex-first').focus();
-            });
+            if (type == 'add') {
+                console.log('adding new transaction...');
+
+                $scope.transaction.date = $scope.dateInput.toJSON();
+
+                $scope.transactions.$add($scope.transaction).then(function(ref) {
+                    var id = ref.key();
+                    // list.$indexFor(id); // returns location in the array
+                    // console.log(id);
+                    $scope.transaction = '';
+                    $scope.dateInput = '';
+                    $('#tabindex-first').focus();
+                });
+            }
+
+            if (type == 'edit') {
+
+                // var list = $firebaseArray(Ref.child('/transactions'));
+                
+                // var transactionRecord = $scope.transactions.getRecord($scope.transaction.$id);
+
+            
+
+                // var item = messages.$getRecord(someRecordKey);
+                // item.user = "alanisawesome";
+                // messages.$save(item).then(function() {
+                  // data has been saved to our database
+                // });
+
+                console.log('saveTransaction - type: edit');
+                console.log($scope.transaction.$id);            
+            }
         };
 
-        $scope.deletetransaction = function(transaction) {
+        $scope.deleteTransaction = function(transaction) {
+            // -----------------------------------------------
             $scope.transactions.$remove(transaction);
         };
+
+        $scope.editTransaction = function(transaction) {
+            $scope.panelControl('edit');
+            
+            $scope.transaction = transaction;
+
+            var ano = Number(transaction.date.substr(0, 4));
+            var mes = Number(transaction.date.substr(5, 2))-1;
+            var dia = Number(transaction.date.substr(8, 2));
+            $scope.dateInput = new Date(ano, mes, dia);
+        };
+
+        $scope.panelControl = function(type) {
+
+            $scope.panelType = type;
+
+            if (type == 'add') {
+                $scope.transaction = '';
+                $scope.dateInput = '';
+                $scope.panelTitle = 'Add Transaction';
+                $scope.openedPanel = true;
+            }
+
+            if (type == 'edit') {
+                $scope.panelTitle = 'Edit Transaction';
+                $scope.openedPanel = true;
+            }
+
+            if (type == 'close') {
+                $scope.openedPanel = false;
+                $scope.panelTitle = '';
+            }
+
+            var setFocus = function() {
+                $('#tabindex-first').focus();
+                console.log('openPanel');
+            }
+
+            setTimeout(setFocus, 500);
+        }
 
         $scope.getTotal = function() {
             var total = 0;
