b6e5b53ce43c11eadf0460c2f11216938015f800
thiagocolen <thiago.souzacolen@gmail.com>
2017-05-13 20:39:49 -0300

    add eslint and babel

--- gulpfile.js
+++ gulpfile.js
@@ -3,57 +3,68 @@ var gulp = require('gulp'),
   reload = browserSync.reload,
   clean = require('gulp-clean'),
   less = require('gulp-less'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+  babel = require('gulp-babel');
 
 
-// Clean WWW Folder 
-gulp.task('clean', function() {
+// Clean WWW Folder
+gulp.task('clean', function () {
   return gulp.src('./build/', { read: false })
     .pipe(clean());
 });
 
 
 // WWW Folder
-gulp.task('dependencies', function() {
-
+gulp.task('dependencies', function () {
   // app
   gulp.src('./src/client/app/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
     .pipe(concat('main.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build/assets/scripts/'));
 
 
   // scripts
   gulp.src([
-      './node_modules/jquery/dist/jquery.js',
-      './node_modules/bootstrap/dist/js/bootstrap.js',
-      './node_modules/angular/angular.js',
-      './node_modules/angular-route/angular-route.js',
-      './node_modules/angular-local-storage/dist/angular-local-storage.js',
-      './node_modules/firebase/firebase.js',
-      './node_modules/angularfire/dist/angularfire.js',
-      './node_modules/chart.js/Chart.js',
-      './node_modules/angular-chart.js/dist/angular-chart.js',
-      './node_modules/angular-hotkeys/build/hotkeys.js'
-    ])
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/bootstrap/dist/js/bootstrap.js',
+    './node_modules/angular/angular.js',
+    './node_modules/angular-route/angular-route.js',
+    './node_modules/angular-local-storage/dist/angular-local-storage.js',
+    './node_modules/firebase/firebase.js',
+    './node_modules/angularfire/dist/angularfire.js',
+    './node_modules/chart.js/Chart.js',
+    './node_modules/angular-chart.js/dist/angular-chart.js',
+    './node_modules/angular-hotkeys/build/hotkeys.js'
+  ])
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('./build/assets/scripts/'));
 
 
-  //css
-  gulp.src(['./src/client/assets/styles/angular-chart.css', './node_modules/angular-hotkeys/build/hotkeys.css'])
+  // css
+  gulp.src([
+    './src/client/assets/styles/angular-chart.css',
+    './node_modules/angular-hotkeys/build/hotkeys.css'
+  ])
     .pipe(gulp.dest('./build/assets/styles/'));
 
-  gulp.src(['./src/client/assets/styles/*.less', './src/client/assets/styles/bootstrap/bootstrap.less'])
+
+  gulp.src([
+    './src/client/assets/styles/*.less',
+    './src/client/assets/styles/bootstrap/bootstrap.less'
+  ])
     .pipe(less())
     .pipe(gulp.dest('./build/assets/styles/'));
 
 
-  //fonts 
+  // fonts
   gulp.src('./node_modules/bootstrap/dist/fonts/*.*')
     .pipe(gulp.dest('./build/assets/fonts/'));
 
 
-  //images 
+  // images
   gulp.src('./src/client/assets/images/*.*')
     .pipe(gulp.dest('./build/assets/images/'));
 
@@ -64,24 +75,21 @@ gulp.task('dependencies', function() {
 
   gulp.src('./src/client/index.html')
     .pipe(gulp.dest('./build/'));
-
-
 });
 
 
-
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: "./build/"
+      baseDir: './build/',
     },
-    open: false
+    open: false,
   });
 });
 
 
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('default', ['browser-sync'], function () {
   gulp.watch(['./src/client/**/*'], ['dependencies', reload]);
   // gulp.watch(['gulpfile.js'], ['dependencies', reload]);
 });
